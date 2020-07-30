c282cf22e001f63ae502ff7d2a9be0a763c84457
ooxx2500 <ooxx2500@gmail.com>
2020-07-30 17:52:14 +0800

    no message

---
+++ python/補充教學.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 30 17:29:06 2020
+
+@author: ASUS
+"""
+'''
+學習資源:
+Git_hub(討論原始碼)
+O'Relly(歐萊禮)
+PyimageSearch
+Java Code Geeks
+stack overflow
+DataCamp
+DataInPoint
+Medium Daily Digest
+Towards Data Science
+
+'''
